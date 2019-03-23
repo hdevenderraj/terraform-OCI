048eb127f3e60c74ab3a50da57c52c2309dc64d2
Devender Raj <devender.hasaji@rubiconred.com>
2019-03-23 21:50:25 +0530

    compartment

---
+++ first-steps/instance.tf
@@ -0,0 +1,17 @@
+provider "oci" {
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaa7z4lnxiww4iznvd27dlpf6qfgpwqozxkgswiwkpgfilsrrzbbjq"
+  user_ocid = "oracleidentitycloudservice/hdevenderraj440@gmail.com"
+  fingerprint = "b2:08:a8:0b:55:22:b0:1e:01:14:16:9c:36:5d:7e:6b"
+  private_key_path = "${var.private_key_path}"
+  region = "eu-frankurt-1"
+}
+
+# Get a list of Availability Domains
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = "${var.tenancy_ocid}"
+}
+
+# Output the result
+output "show-ads" {
+  value = "${data.oci_identity_availability_domains.ads.availability_domains}"
+}
