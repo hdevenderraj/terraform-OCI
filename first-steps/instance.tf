f7bd9279e3f4721e5dd91da5d7dd4cf88ec34a6a
hdevenderraj <hdevenderraj@gmail.com>
2019-03-23 22:26:10 +0530

    1st

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,9 +1,9 @@
 provider "oci" {
-  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaa7z4lnxiww4iznvd27dlpf6qfgpwqozxkgswiwkpgfilsrrzbbjq"
-  user_ocid = "oracleidentitycloudservice/hdevenderraj440@gmail.com"
-  fingerprint = "b2:08:a8:0b:55:22:b0:1e:01:14:16:9c:36:5d:7e:6b"
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  fingerprint = "${var.fingerprint}"
   private_key_path = "${var.private_key_path}"
-  region = "eu-frankurt-1"
+  region = "${var.region}"
 }
 
 # Get a list of Availability Domains
